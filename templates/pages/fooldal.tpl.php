c24c71aa672ae8405253521cbcc529b4f29aab54
MAndor_AKYY5Z <makaiandor52@gmail.com>
2026-04-19 17:09:33 +0200

    Főoldal
    Elkészült a főoldal. Design még hiányzik!

---
+++ templates/pages/fooldal.tpl.php
@@ -0,0 +1,86 @@
+<div id="demo" class="carousel slide" data-bs-ride="carousel">
+
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+        <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+        <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+    </div>
+
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="./images/siofok.jpg"
+                 alt="Webfejlesztés"
+                 class="d-block w-100">
+            <div class="carousel-caption">
+                <h3>Bahart</h3>
+                <p>Siófok katamarán</p>
+            </div>
+        </div>
+
+        <div class="carousel-item">
+            <img src="./images/fured.jpg"
+                 alt="Kód szerkesztés"
+                 class="d-block w-100">
+            <div class="carousel-caption">
+                <h3>Bahart</h3>
+                <p>Füred katamarán</p>
+            </div>
+        </div>
+
+        <div class="carousel-item">
+            <img src="./images/badacsony.jpg"
+                 alt="Programozás"
+                 class="d-block w-100">
+            <div class="carousel-caption">
+                <h3>Bahart</h3>
+                <p>Badacsony katamarán</p>
+            </div>
+        </div>
+    </div>
+
+    <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+    </button>
+
+    <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+        <span class="carousel-control-next-icon"></span>
+    </button>
+
+</div>
+<div class="fooldal">
+    <h2>Balatoni hajók</h2>
+    <p>A balatoni hajózás a XIX. század közepétől követhető nyomon, amikor megkezdődött a rendszeres gőzhajózás a tó medencéjén. Ma programunkban részletesen megismerheti a jelenleg forgalomban lévő és a már kivont hajókat, az úszó munkagépeket, valamint a névutóbbiak történelmi név-változtatásait. A tulajdonosok adatai is rendelkezésre állnak, ha azok ismertek, így átfogó képet kap a balatoni vízi közlekedés teljes palettájáról.</p>
+    <h3>Média tartalom</h3>
+    <div class="video-row">
+        <div class="video-item">
+            <p>Saját videó</p>
+            <video width="640" controls>
+                <source src="./images/sajat_video.mp4" type="video/mp4">
+                A böngésződ nem támogatja a videólejátszást.
+            </video>
+        </div>
+        <div class="video-item">
+            <p>Youtube Videó</p>
+            <iframe
+                width="640"
+                height="360"
+                src="https://www.youtube.com/embed/gBBsS7hhrhw"
+                title="YouTube videó"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen>
+            </iframe>
+        </div>
+    </div>
+    <h3>Google Térkép</h3>
+    <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2706.8509470385686!2d18.044254!3d46.9088821!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4769c09c888496fb%3A0xe0d1e2be6aca1072!2sBalatoni%20Haj%C3%B3z%C3%A1si%20Zrt.!5e0!3m2!1shu!2shu!4v1713450000000"
+        width="600"
+        height="450"
+        style="border:0;"
+        allowfullscreen=""
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade">
+    </iframe>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
