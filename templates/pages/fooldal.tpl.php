4b2c6516477c7a5b979ca6588a4c8900d85ac8a1
MAndor_AKYY5Z <makaiandor52@gmail.com>
2026-04-22 10:17:54 +0200

    Javítás
    Főoldal-ra került be több HTML5-ös tag a feladatkövetelmény alapján.

--- templates/pages/fooldal.tpl.php
+++ templates/pages/fooldal.tpl.php
@@ -47,36 +47,38 @@
     </button>
 
 </div>
+
 <div class="fooldal">
     <h2>Balatoni hajók</h2>
     <p>A balatoni hajózás a XIX. század közepétől követhető nyomon, amikor megkezdődött a rendszeres gőzhajózás a tó medencéjén. Ma programunkban részletesen megismerheti a jelenleg forgalomban lévő és a már kivont hajókat, az úszó munkagépeket, valamint a névutóbbiak történelmi név-változtatásait. A tulajdonosok adatai is rendelkezésre állnak, ha azok ismertek, így átfogó képet kap a balatoni vízi közlekedés teljes palettájáról.</p>
     <h3>Média tartalom</h3>
-    <div class="video-row">
-        <div class="video-item">
-            <p>Saját videó</p>
-            <video width="640" controls>
-                <source src="./images/sajat_video.mp4" type="video/mp4">
-                A böngésződ nem támogatja a videólejátszást.
-            </video>
-        </div>
-        <div class="video-item">
-            <p>Youtube Videó</p>
-            <iframe
-                width="640"
-                height="360"
-                src="https://www.youtube.com/embed/gBBsS7hhrhw"
-                title="YouTube videó"
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen>
-            </iframe>
-        </div>
+
+    <div class="videos">
+        <article class="video-row">
+            <div class="video-item">
+                <p>Saját videó</p>
+                <video controls>
+                    <source src="./images/sajat_video.mp4" type="video/mp4">
+                    A böngésződ nem támogatja a videólejátszást.
+                </video>
+            </div>
+        </article>
+
+        <aside class="video-row">
+            <div class="video-item">
+                <p>Youtube videó</p>
+                <iframe
+                    src="https://www.youtube.com/embed/gBBsS7hhrhw"
+                    title="YouTube videó"
+                    allowfullscreen>
+                </iframe>
+            </div>
+        </aside>
     </div>
+
     <h3>Térképes elérhetőség</h3>
     <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2706.8509470385686!2d18.044254!3d46.9088821!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4769c09c888496fb%3A0xe0d1e2be6aca1072!2sBalatoni%20Haj%C3%B3z%C3%A1si%20Zrt.!5e0!3m2!1shu!2shu!4v1713450000000"
-        width="600"
-        height="450"
         style="border:0;"
         allowfullscreen=""
         loading="lazy"
